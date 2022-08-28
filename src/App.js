68c16706dcc5969a5188b1e0b26c5df1d0d647cc
Simon <Monkeymandeveloper@protonmail.com>
2022-08-28 11:21:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import JSONDATA from "./Users.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [info, setInfo] = useState([
+    {
+      fullName: "Elvera Thackston",
+      departement: "Human Resources",
+      worksAt: "Rosenbaum Inc",
+      id: 1,
+    },
+    {
+      fullName: "Verge Gherardesci",
+      departement: "Training",
+      worksAt: "Becker-Adams",
+      id: 2,
+    },
+    {
+      fullName: "Farica Chalfant",
+      departement: "Support",
+      worksAt: "Schimmel, Wiza and Aufderhar",
+      id: 3,
+    },
+    {
+      fullName: "Elwin Guinane",
+      departement: "Business Development",
+      worksAt: "Koepp, Swaniawski and Ritchie",
+      id: 4,
+    },
+    {
+      fullName: "Torr Tappington",
+      departement: "Business Development",
+      worksAt: "Klein-Stiedemann",
+      id: 5,
+    },
+  ]);
+
+  return <div className="App">
+    {info.map((info) =>(
+      <div className="info-preview" key={info.id} >
+        <h1>{ info.fullName }</h1>
+        <p>Works in: <span>{info.departement}</span></p>
+        <p>Company: <span>{info.worksAt}</span></p>
+      </div>
+    ))}
+  </div>;
 }
 
 export default App;
