a83d652ab526f84b07e36b1a795b0a76bc317c78
Simon <Monkeymandeveloper@protonmail.com>
2022-08-28 16:18:50 +0200

    Info re-arrangement

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import "./App.css";
 /* import { useState } from "react"; */
 import JSONDATA from "./Users.json";
+import { BigHead } from '@bigheads/core';
 
 function App() {
   return (
     <div className="App">
+      {/* <BigHead /> */}
       {JSONDATA.map((info, id) => (
         <div className="user-preview" key={id}>
           <div className="profile-picutre">
-            <img src={info.avatar} alt="avatar" />
-          </div>
-          <div className="info">
-            <div className="main-info">
+          <img src={info.avatar} alt="avatar" />
+            <BigHead />
+          </div>     
+          <div class="right-side">
+            <div className="name">
               <h1>{info.fullName}</h1>
-              <h2>Gender: {info.gender}</h2>
-              <p className="language">First Language : {info.mainLanguage}</p>
-              <p className="work-field">
-                Works in: <span>{info.departement}</span>
-              </p>
             </div>
-            <div className="second-based-info">
-              <p className="company">
-                Company: <span>{info.worksAt}</span>
-              </p>
-              <p className="education">
-                Higher education at: <span>{info.higherEducation}</span>
-              </p>
-              <p className="password">
-                password <span>{info.password}</span>
-              </p>
+            <div className="info">
+              <div className="main-info">
+                <h2>Personnal Info</h2>
+                <p className="gender ">Gender: <span>{info.gender}</span></p>
+                <p className="language">First Language : <span>{info.mainLanguage}</span></p>
+                <p className="password">
+                  password <span>{info.password}</span>
+                </p>
+              </div>
+              <div className="second-based-info">
+                <h2>Professional Info</h2>
+                <p className="company">
+                  Company: <span>{info.worksAt}</span>
+                </p>
+                <p className="work-field">
+                  Works in: <span>{info.departement}</span>
+                </p>
+                <p className="education">
+                  Higher education at: <span>{info.higherEducation}</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
